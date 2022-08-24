41ea2b9b3107ce7f612357161105690b033e61ac
darap1 <darap@mail.ru>
2022-08-25 01:57:11 +0200

    feat : add types, uncomment login code

--- src/app/views/loginPage/login.ts
+++ src/app/views/loginPage/login.ts
@@ -4,6 +4,7 @@ import { IView, MenuBtn } from '../interfaces';
 import { innerText } from '../mainPage/mainPageHtml';
 import { View } from '../view';
 import './login.css';
+import { IUserSignIn, INewUserRegistration, IAutentificatedUser } from './types';
 
 export class Login extends View implements IView {
     private btn: MenuBtn;
@@ -55,129 +56,106 @@ export class Login extends View implements IView {
       autorization.classList.add('autorization');
       autorization.innerHTML = autorizationForm;
       autorization.appendChild(btn);
+      this.renderTorefractorCode();
       return [autorization];
     }
   
     protected getBtnToChangePage(): MenuBtn {
       return this.btn;
     }
+
+
+
+
+  renderTorefractorCode() {
+    
+
+
+
+    let signinUser:IUserSignIn ={
+      email:'',
+      password:'',
+    }
+    let registrNewUser:INewUserRegistration={
+      email:'',
+      password:'',
+      name:'',
+    };
+    let autorizedUser:IAutentificatedUser;
+    let errorEmail:string = 'incorect email';
+
+    const createUser = async (user:INewUserRegistration) => {
+            const rawResponse = await fetch(this.urlLocalUsersCreate, {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(user)
+            });
+           const content = await rawResponse.json();
+          console.log(content);
+    };
+
+   const loginUser = async (user:IUserSignIn) => {
+    const rawResponse = await fetch(this.urlLocalSignIn, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+
+    return rawResponse.json();
   }
 
+    document.body.addEventListener('submit',(submitFormHandler));
 
-
-// class Login extends View {
-//   static TextObject = {
-//     MainTitle: 'Login',
-//   };
-
-//   constructor() {
-//     super();
-//   }
-
-//   render() {
-//     
-//     type INewUserRegistration= {
-//       name:string;
-//       email:string;
-//       password:string;
-//     }
-
-//     type IUserSignIn= {
-//       email:string;
-//       password:string;
-//     }
-
-//     type IAutentificatedUser= {
-//       message:string;
-//       name:string;
-//       refreshToken:string;
-//       token:string;
-//       userId:string;
-//     }
-
-
-
-//     let signinUser:IUserSignIn ={
-//       email:'',
-//       password:'',
-//     }
-//     let registrNewUser:INewUserRegistration={
-//       email:'',
-//       password:'',
-//       name:'',
-//     };
-//     let autorizedUser:IAutentificatedUser;
-//     let errorEmail:string = 'incorect email';
-
-//     const createUser = async (user:INewUserRegistration) => {
-//             const rawResponse = await fetch(urlLocalUsersCreate, {
-//               method: 'POST',
-//               headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//               },
-//               body: JSON.stringify(user)
-//             });
-//            const content = await rawResponse.json();
-//           console.log(content);
-//     };
-
-//    const loginUser = async (user:IUserSignIn) => {
-//     const rawResponse = await fetch(urlLocalSignIn, {
-//       method: 'POST',
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(user)
-//     });
-
-//     return rawResponse.json();
-//   }
-
-//     document.body.addEventListener('submit',(submitFormHandler));
-
-//     function submitFormHandler(event:Event){
-//       event.preventDefault();
-//       const userSignIn = document.getElementById('signin') as HTMLInputElement;
-//       const userSignUp = document.getElementById('signup') as HTMLInputElement;
-//       const emailForm =document.getElementById('email') as HTMLInputElement;
-//       const passwordForm =document.getElementById('pass')as HTMLInputElement;
-//       const name =document.getElementById('name')as HTMLInputElement;
-//       const loginText = document.querySelector('.login') as HTMLElement;
-//       if(userSignIn.checked){
-//       console.log(emailForm.value,passwordForm.value);
-//       signinUser.email = emailForm.value;
-//       signinUser.password = passwordForm.value;
-//       loginUser(signinUser).then(response =>{
-//         autorizedUser = response;
-//         console.log(autorizedUser.token)})
-//         .then(()=>alert('вы авторизированны'))
-//         .then(()=>{
-//         App.renderNewPage('main-page');
-//         loginText!.innerHTML = autorizedUser.name;
-//           localStorage.setItem('token',autorizedUser.token);
-//           localStorage.setItem('id',autorizedUser.userId);
-//           localStorage.setItem('name',autorizedUser.name);
-//           window.location.href='';
+    function submitFormHandler(event:Event){
+      event.preventDefault();
+      const userSignIn = document.getElementById('signin') as HTMLInputElement;
+      const userSignUp = document.getElementById('signup') as HTMLInputElement;
+      const emailForm =document.getElementById('email') as HTMLInputElement;
+      const passwordForm =document.getElementById('pass')as HTMLInputElement;
+      const name =document.getElementById('name')as HTMLInputElement;
+      const loginText = document.querySelector('.login') as HTMLElement;
+      //name.required = false;
+      if(userSignIn.checked){
+      console.log(emailForm.value,passwordForm.value);
+      signinUser.email = emailForm.value;
+      signinUser.password = passwordForm.value;
+      loginUser(signinUser).then(response =>{
+        autorizedUser = response;
+        console.log(autorizedUser.token)})
+        .then(()=>alert('вы авторизированны'))
+        .then(()=>{
+       // App.renderNewPage('main-page');
+        loginText!.innerHTML = autorizedUser.name;
+          localStorage.setItem('User',JSON.stringify(autorizedUser));
+          window.location.href='#';
         
-//         })
-//         .catch(()=>console.log('не правильные данные'));
-//       }
-//       if(userSignUp.checked){
-//         console.log(emailForm.value,passwordForm.value);
-//         registrNewUser.email = emailForm.value;
-//         registrNewUser.password = passwordForm.value;
-//         registrNewUser.name = name.value;
-//         createUser(registrNewUser)
-//         .then(()=>alert('вы зарегистрированны'))
-//         .catch(()=>console.log('не правильные данные'));
-//       }
-//     }
+        })
+        .catch(()=>console.log('не правильные данные'));
+      }
+      if(userSignUp.checked){
+        //name.value===''?name.required=true:name.required=false;
+        if(name.value.toString() !==''){
+        console.log(emailForm.value,passwordForm.value);
+        registrNewUser.email = emailForm.value;
+        registrNewUser.password = passwordForm.value;
+        registrNewUser.name = name.value;
+        createUser(registrNewUser)
+        .then(()=>{alert('Вы зарегистрированны');localStorage.setItem('User',JSON.stringify(registrNewUser));window.location.href='#';})
+        .catch(()=>alert('Такой пользователь уже есть'));
+        }else (alert('Введите имя'))
+      }
+    }
 
-//     this.container.innerHTML=html;
-//     return this.container;
-//   }
-// }
+    // this.container.innerHTML=html;
+    // return this.container;
+  }
+
+}
 
 export default Login;
