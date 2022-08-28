b3152b0c2bbec574f6aa13a4fddfee285561ffe4
darap1 <darap@mail.ru>
2022-08-28 14:49:01 +0200

    feat : add footer btn name from local storage

--- src/app/views/header/header.ts
+++ src/app/views/header/header.ts
@@ -1,12 +1,15 @@
 import Component from '../components/components';
 import './header.css';
 import { ViewPath } from '../../common/constants';
+import { IAutentificatedUser } from '../loginPage/types';
 
 class Header extends Component {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(tagName: string, className: string, id?: string) {
     super(tagName, className, id);
   }
 
+  // eslint-disable-next-line max-lines-per-function
   render() {
     this.container.innerHTML = `
     <div class='container'>
@@ -32,14 +35,14 @@ class Header extends Component {
            <li><a class="header__link" href="#${ViewPath.STATISTICS}">Статистика</a></li>
            <li><a class="header__link" href="${ViewPath.ABOUT}">О приложении</a></li>
            <li><a class="header__link" href="${ViewPath.TEAM}">О команде</a></li>
-           <li><a class="header__link login" href="#${ViewPath.LOGIN}">Login</a></li>
+           <li><a class="header__link login" href="#${ViewPath.LOGIN}">${this.headerBthText()}</a></li>
          </ul>
         </nav>
        </div>
     </div>
   `;
     document.body.addEventListener('click', function (event) {
-      const eventTarget = event!.target! as HTMLButtonElement;
+      const eventTarget = event.target as HTMLButtonElement;
       if (eventTarget.classList.contains('header__burger')) {
         eventTarget.classList.toggle('active');
         const menu = document.getElementsByClassName('header__menu') as HTMLCollection;
@@ -48,6 +51,19 @@ class Header extends Component {
     });
 
     return this.container;
+  }
+
+  private headerBthText(): string {
+    let headerBthText: string;
+    const user: IAutentificatedUser | null = localStorage.getItem('user')
+      ? <IAutentificatedUser>JSON.parse(localStorage.getItem('user') || '')
+      : null;
+    if (user === null) {
+      headerBthText = 'Login';
+    } else {
+      headerBthText = user.name;
+    }
+    return headerBthText;
   }
 }
 
