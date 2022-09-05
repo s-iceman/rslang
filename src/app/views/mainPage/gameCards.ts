f46c5ba51bfb80c87781d515261c5a8040850147
darap1 <darap@mail.ru>
2022-09-05 16:53:32 +0200

    refactor: img path

--- src/app/views/mainPage/gameCards.ts
+++ src/app/views/mainPage/gameCards.ts
@@ -4,7 +4,7 @@ const renderSprintCard = () => `
   <div class="why-us__box game-card" id="${ViewPath.SPRINT}">
     <span></span>
     <h4>Спринт</h4>
-    <div class="why-us__box_img"><img src="../assets/img/why-as-box/sprint.jpg" alt="statistic"></div>
+    <div class="why-us__box_img"><img src="./assets/img/why-as-box/sprint.jpg" alt="statistic"></div>
     <p>Запоминай быстрее.</p>
   </div>
 `;
@@ -13,7 +13,7 @@ const renderVoiceCallCard = () => `
   <div class="why-us__box game-card" id="${ViewPath.VOICECALL}">
     <span></span>
     <h4>Аудиовызов</h4>
-    <div class="why-us__box_img"><img src="../assets/img/why-as-box/savana.jpg" alt="statistic"></div>
+    <div class="why-us__box_img"><img src="./assets/img/why-as-box/savana.jpg" alt="statistic"></div>
     <p>Играй и учись</p>
   </div>
 `;
